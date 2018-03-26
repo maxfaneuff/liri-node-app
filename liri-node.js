903473a086a8ba760b4519ddfce7ca1bc895f7dd
Max Faneuff <maxfaneuff@gmail.com>
2018-03-25 22:02:01 -0400

    finished twitter, spotify.  starting omdb

--- liri-node.js
+++ liri-node.js
@@ -1,8 +1,101 @@
 var dotenv = require("dotenv").config();
-var keys = require("keys.js");
-var twitter = require("twitter");
-var rspotify = require("node-spoitify-api");
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var twitterHanle = "maxfaneuff";
 
-console.log("working");
+
+var args = process.argv;
+
+//Twitter//
+function getTweets() {
+
+	client.get('statuses/user_timeline', {screen_name: 'maxfaneuff'}, function(error, tweets, response) {
+		if (error) throw error;
+		console.log("TWEETS BY @MAXFANEUFF");
+		// console.log(JSON.stringify(tweets, null, 2));
+		for (var i = 0; i < 20; i++){
+		console.log("\n");
+		console.log("Tweeted on " + tweets[i].created_at);
+		console.log(JSON.stringify(tweets[i].text, null, 2));
+		console.log("________________________________________________________________________________________");
+		}
+	});
+}
+
+//Spotify//
+function getSpotify(song) {
+	spotify
+	  .search({ type: 'track', query: song })
+	  .then(function(response) {
+	  	for (var i = 0; i < 10; i++){
+	  	console.log("________________________________________________________________________________________")
+	  	console.log("10 Results for Songs Titled:  " + song);
+	    console.log("\n");
+	    console.log("********* ARTIST NAME:  ********");
+	    console.log(response.tracks.items[i].artists[0].name);
+	    console.log("********* TRACK NAME:  **********");	
+	    console.log(response.tracks.items[i].name);
+	    console.log("********** PREVIEW LINK:  ********");
+	    if (response.tracks.items[i].preview_url != null) {	
+	    console.log(response.tracks.items[i].preview_url);
+	    } else {
+	    console.log("Sorry, no preview available...");
+	    }
+	    console.log("********** ALBUM NAME:  **********");
+	    console.log(response.tracks.items[i].album.name);
+	    console.log("\n");
+
+	  	}
+	  })
+	  .catch(function(err) {
+	    console.log(err);
+	  });
+}
+
+function getMovie(movieName) {
+	var request = require('request');
+	var queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movieName;
+	console.log(queryURL);
+	request(queryURL, function (error, response, body) {
+		if(error){
+			console.log('error:', error);
+		} 
+	  	console.log(response); 
+	});
+}
+
+
+//User-input control//
+if (args[2] === "my-tweets") {
+	getTweets();
+} else if (args[2] === "spotify-this-song") {
+	var songArr = [];
+	var songName;
+	if(args[3] != "") {
+		for(var i = 3; i<args.length; i++) {
+			songArr.push(args[i]);
+			songName = songArr.join("+");
+		} 
+		//This still needs work//
+	} else if (args[3] == undefined) {	
+		songName = "The+Sign+Ace+of+Base";
+	}	
+	console.log(songName);
+	getSpotify(songName);
+} else if (args[2] == "movie-this") {
+	var movieArr = [];
+	var movieName
+	if(args[3] != "") {
+		for (var i = 3; i<args.length; i++) {
+			movieArr.push(args[i]);
+			movieName = movieArr.join("+");
+		}
+	} else if(args.length == 2) {
+		movieName = "Mr+Nobody";
+	}
+	console.log(movieName);
+	getMovie();
+}
